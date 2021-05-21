bc4ea5b1944712d05957b55c976b45fd105efb61
nsurendran1991 <82565509+nsurendran1991@users.noreply.github.com>
2021-05-21 13:26:13 +1000

    Update MavenProjectApplicationTests.java

--- src/test/java/com/example/demo/MavenProjectApplicationTests.java
+++ src/test/java/com/example/demo/MavenProjectApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class MavenProjectApplicationTests {
 
 	
