b52c5ac59e2c73529a60b02be46049feeb23c13e
nsurendran1991 <82565509+nsurendran1991@users.noreply.github.com>
2021-05-21 14:18:57 +1000

    Update MavenProjectApplicationTests.java

--- src/test/java/com/example/demo/MavenProjectApplicationTests.java
+++ src/test/java/com/example/demo/MavenProjectApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
